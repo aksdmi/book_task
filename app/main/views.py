f6c01e12d262e1f038290217cfdc42d81ef89c12
Dmitry Aksenov <aksdmi@gmail.com>
2023-03-16 14:59:23 +0300

    Added delete views. Change templates.

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,6 @@ from ..models import Book, Author, Publisher
 from . forms import BookForm, AuthorForm, PublisherForm
 
 
-
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = BookForm()
@@ -21,6 +20,7 @@ def index():
         error_out=False)
     books = pagination.items
     return render_template('index.html', form=form, books=books, pagination=pagination)
+
 
 def insert_update_book(form: BookForm, book: Book, msg: str, endpoint: str):
     book.title = form.title.data
@@ -71,7 +71,8 @@ def edit(id):
         form.publishers.choices.append((row.id, row.name))
 
     if form.validate_on_submit():
-        return insert_update_book(form, book, 'Данные о книге были обновлены.', '.index')
+        msg = f'Данные о книге "{book.title}" были обновлены.'
+        return insert_update_book(form, book, msg, '.index')
 
     form = update_defaults(form, book)
 
@@ -79,6 +80,16 @@ def edit(id):
     form.year.data = book.year
 
     return render_template('edit_book.html', form=form)
+
+@main.route('/delete/<int:id>', methods=['GET'])
+def delete(id):
+    book = Book.query.get_or_404(id)
+    msg = f'Данные книги {book.title} удалены'
+    db.session.delete(book)
+    db.session.commit()
+    flash(msg)
+    return redirect(url_for('.index'))
+
 
 @main.route('/new', methods=['GET', 'POST'])
 def new():
@@ -96,7 +107,8 @@ def new():
         form.publishers.choices.append((row.id, row.name))
 
     if form.validate_on_submit():
-        return insert_update_book(form, book, 'Данные о книге были добавлены.', '.index')
+        msg = f'Данные о книге {book.title} были добавлены.'
+        return insert_update_book(form, book, msg, '.index')
 
     form = update_defaults(form, book)
 
@@ -113,12 +125,14 @@ def authors():
 def authors_new():
     form = AuthorForm()
     if form.validate_on_submit():
+        msg = f'Добавлен новый автор "{form.fio.data}"'
+
         author = Author()
         author.fio = form.fio.data
 
         db.session.add(author)
         db.session.commit()
-        flash('Добавлен новый автор')
+        flash(msg)
         return redirect(url_for('.authors'))
 
     return render_template('edit_author.html', form=form)
@@ -129,17 +143,27 @@ def authors_edit(id):
     form = AuthorForm()
 
     if form.validate_on_submit():
-        author.fio = form.fio.data
 
+        msg = f'Обновлен автор "{author.fio}" -> "{form.fio.data}"'
+        author.fio = form.fio.data
         db.session.add(author)
         db.session.commit()
-        flash('Обновлен автор')
+        flash(msg)
         return redirect(url_for('.authors'))
 
     form.fio.data = author.fio
 
     return render_template('edit_author.html', form=form)
 
+@main.route('/authors/delete/<int:id>', methods=['GET'])
+def authors_delete(id):
+    author = Author.query.get_or_404(id)
+
+    msg = f'Удален автор "{author.fio}"'
+    db.session.delete(author)
+    db.session.commit()
+    flash(msg)
+    return redirect(url_for('.authors'))
 
 @main.route('/publishers', methods=['GET'])
 def publishers():
@@ -151,12 +175,14 @@ def publishers():
 def publishers_new():
     form = PublisherForm()
     if form.validate_on_submit():
+        msg = f'Добавлен новый издатель "{form.name.data}"'
+
         publisher = Publisher()
         publisher.name = form.name.data
 
         db.session.add(publisher)
         db.session.commit()
-        flash('Добавлен новый издатель')
+        flash(msg)
         return redirect(url_for('.publishers'))
 
     return render_template('edit_publisher.html', form=form)
@@ -167,13 +193,27 @@ def publishers_edit(id):
     form = PublisherForm()
 
     if form.validate_on_submit():
+        msg = f'Обновлен издатель "{publisher.name}" -> "{form.name.data}"'
+
         publisher.name = form.name.data
+
+
 
         db.session.add(publisher)
         db.session.commit()
-        flash('Обновлен издатель')
+        flash(msg)
         return redirect(url_for('.publishers'))
 
     form.name.data = publisher.name
 
     return render_template('edit_publisher.html', form=form)
+
+@main.route('/publishers/delete/<int:id>', methods=['GET'])
+def publishers_delete(id):
+    publisher = Publisher.query.get_or_404(id)
+
+    msg = f'Удален издатель "{publisher.name}"'
+    db.session.delete(publisher)
+    db.session.commit()
+    flash(msg)
+    return redirect(url_for('.publishers'))
