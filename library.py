39a65ff6b696174d9df6bdb45b991569fbc8cb66
Dmitry Aksenov <aksdmi@gmail.com>
2023-03-17 22:50:46 +0300

    Some changes

--- library.py
+++ library.py
@@ -5,10 +5,9 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models import Book, Author, Publisher
-
 
 app = create_app('production')
 migrate = Migrate(app, db)
